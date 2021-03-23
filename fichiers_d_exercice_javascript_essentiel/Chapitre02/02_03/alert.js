49cb9e9ba9c09bb3479366a989be904c9d9db859
Laby Damaro Camara <ldamaro98@gmail.com>
2021-03-23 19:19:39 +0100

    commentaire  en JS

--- fichiers_d_exercice_javascript_essentiel/Chapitre02/02_03/alert.js
+++ fichiers_d_exercice_javascript_essentiel/Chapitre02/02_03/alert.js
@@ -1,1 +1,5 @@
-alert("Bonjour de javascript!");
+//alert("Bonjour de javascript!");
+console.log("Bonjour de javascript!");
+console.info("Bonjour info");
+console.warn("Bonjour Warn");
+console.error("Bonjour Error");
