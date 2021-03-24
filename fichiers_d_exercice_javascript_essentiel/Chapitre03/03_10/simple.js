134e318227a207cac0479263bc1878bfa8f4c856
Laby Damaro Camara <ldamaro98@gmail.com>
2021-03-24 03:37:51 +0100

    chapitre 3

--- fichiers_d_exercice_javascript_essentiel/Chapitre03/03_10/simple.js
+++ fichiers_d_exercice_javascript_essentiel/Chapitre03/03_10/simple.js
@@ -2,3 +2,4 @@ var colors = ["Rouge", "Vert", "Bleu"];
 
 
 console.log(colors[5]);
+console.table(colors)
---
+++ fichiers_d_exercice_javascript_essentiel/Chapitre03/03_11/simple.js
@@ -0,0 +1,7 @@
+var person = new Object();
+person.firstName = "Laby Damaro";
+person.lastName = "Camara";
+person.age = 27;
+person.sex = "M";
+
+console.log(person)
